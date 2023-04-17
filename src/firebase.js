a59e93163579719f95f48388960331d5fb00056a
Sohteum <thtwk1123@gmail.com>
2023-04-18 08:19:18 +0900

    .

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5E1UFW0RJd3hocatvOnf9nXB0S0pMguI",
+  authDomain: "nwitter-9a45a.firebaseapp.com",
+  projectId: "nwitter-9a45a",
+  storageBucket: "nwitter-9a45a.appspot.com",
+  messagingSenderId: "190450509375",
+  appId: "1:190450509375:web:981f8a5922dce8ba927a8a"
+};
+
+firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
